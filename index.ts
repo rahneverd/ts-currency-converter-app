9fecad8d41bbf425b3741d13eefe71ebadf2dd1c
Muhammad Owais <rahneverd@gmail.com>
2024-04-23 08:21:47 +0300

    done

---
+++ index.ts
@@ -0,0 +1,62 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+    const currencyRates = [
+        {
+            Currency: "USD",
+            Rate: 1
+        },
+        {
+            Currency: "PKR",
+            Rate: 277
+        },
+        {
+            Currency: "EUR",
+            Rate: 0.85
+        },
+        {
+            Currency: "GBP",
+            Rate: 0.72
+        }
+    ]
+
+    const fromCurrencyPrompt = await inquirer.prompt([
+        {
+            type: "list",
+            name: "fromCurrency",
+            message: "Select the currency you want to convert from:",
+            choices: currencyRates.map(rate => rate.Currency)
+        }
+    ]);
+
+    const toCurrencyPrompt = await inquirer.prompt([
+        {
+            type: "list",
+            name: "toCurrency",
+            message: "Select the currency you want to convert to:",
+            choices: currencyRates.map(rate => rate.Currency).filter(currency => currency !== fromCurrencyPrompt.fromCurrency)
+        }
+    ]);
+
+    const amountPrompt = await inquirer.prompt([
+        {
+            type: "number",
+            name: "amount",
+            message: "Enter the amount you want to convert:",
+            filter: (input: string) => parseInt(input),
+            validate: (input: string) => !isNaN(parseFloat(input))
+        }
+    ]);
+
+    const fromCurrency = currencyRates.find(rate => rate.Currency === fromCurrencyPrompt.fromCurrency);
+    const toCurrency = currencyRates.find(rate => rate.Currency === toCurrencyPrompt.toCurrency);
+
+    if (!fromCurrency || !toCurrency) {
+        console.log("Invalid currency selection!");
+    } else {
+        const convertedAmount = (amountPrompt.amount * (toCurrency.Rate / fromCurrency.Rate));
+
+        console.log(`The converted amount is: ${convertedAmount}`);
+    }
+    
